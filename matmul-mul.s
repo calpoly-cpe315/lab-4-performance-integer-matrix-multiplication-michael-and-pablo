1d0b174ad553353573ce2a4bac0f8971673f6676
Pablo Gonzalez <pgonza25@arm.csc.calpoly.edu>
2022-03-01 20:13:04 -0500

    new version of matmul-mul

--- matmul-mul.s
+++ matmul-mul.s
@@ -21,73 +21,59 @@ x28: k
    .global matmul
 matmul:
    // save registers
-   stp x29, x30, [sp, -96]!
-   stp x19, x20, [sp, 16]
-   stp x21, x22, [sp, 32]
-   stp x23, x24, [sp, 48]
-   stp x25, x26, [sp, 64]
-   stp x27, x28, [sp, 80]
+   stp x29, x30, [sp, -16]!
 
    // save copy of args
-   mov x19, x0
-   mov x20, x1
-   mov x21, x2
-   mov x22, x3
-   mov x23, x4
-   mov x24, x5
 
-   mov x26, #0 //init loop variables to 0
+   mov x7, #0 //init loop variables to 0
+//x6, x7: sum, i
+//x13, x14: j, k
 
 iloop:
-   cmp x26, x22 //compare i and hA
+   cmp x7, x3 //compare i and hA
    b.ge end //when i >= hA, go to end
-   mov x27, #0
+   mov x13, #0
 
 jloop:
-   cmp x27, x24  //compare j and wB
+   cmp x13, x5  //compare j and wB
    b.ge addi //increment i once j loop has finished iterating
-   mov x25, #0
-   mov x28, #0
+   mov x6, #0
+   mov x14, #0
 
 kloop:
-   cmp x28, x23 
+   cmp x14, x4 
    b.ge setres // once k == wA, branch
    //sum += A[i * wA + k] * B[k * wB + 
-   mul x18, x26, x23
-   add x18, x18, x28
-   mov x0, #4
-   mul x18, x18, x0
-   ldr w20, [x20, x18]
-   mul x18, x28, x24
-   add x18, x18, x28
-   mov x0, #4
-   mul x18, x18, x0
-   ldr w21, [x21, x18]
-   mul x18, x21, x20
-   add x25, x25, x18
-   add x28, x28, #1
+   mul x9, x7, x4
+   add x9, x9, x14
+   mov x10, #4
+   mul x9, x9, x10
+   ldr x11, [x1, x9]
+   mul x9, x14, x5
+   add x9, x9, x14
+   mov x10, #4
+   mul x9, x9, x10
+   ldr x12, [x2, x9]
+   mul x9, x12, x11
+   add x6, x6, x9
+   add x14, x14, #1
    b kloop
 
 setres:
    //C[i * wB + j] = sum;
    //i * wB + j
-   mul x18, x26, x24 //i * wB
-   add x18, x18, x27 //i * wB + j
-   mov x0, #4
-   mul x18, x18, x0
-   str w25, [x19, x18]
-   add x27, x27, #1
+   mul x9, x7, x5 //i * wB
+   add x9, x9, x13 //i * wB + j
+   mov x10, #4
+   mul x9, x9, x10
+   str w6, [x0, x9]
+   add x13, x13, #1
    b jloop
 
 addi:
-   add x26, x26, #1
+   add x7, x7, #1
    b iloop
 
 end:
-   ldp x19, x20, [sp, 16]
-   ldp x21, x22, [sp, 32]
-   ldp x23, x24, [sp, 48]
-   ldp x25, x26, [sp, 64]
-   ldp x27, x28, [sp, 80]
-   ldp x29, x30, [sp], 96
+   ldp x29, x30, [sp], 16
    ret
